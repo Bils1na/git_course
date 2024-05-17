2d791e4d074a681aff2c0d2dc22c088b6ca4aa77
bilsina <bils1n@icloud.com>
2024-05-17 14:14:09 +0500

    added sum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(sum(1, 4));
+        System.out.println(sum(1, 10));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
