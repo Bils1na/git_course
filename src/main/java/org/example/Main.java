6b769e383692054df63b3252a1c74c554a49e102
bilsina <bils1n@icloud.com>
2024-05-17 14:16:41 +0500

    added div

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(sum(1, 4));
         System.out.println(sum(1, 10));
         System.out.println(subtract(100, 10));
+        System.out.println(div(100, 1000));
     }
 
     public static int sum(int a, int b) {
@@ -14,5 +15,9 @@ public class Main {
 
     public static int subtract(int a, int b) {
         return a - b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
     }
 }
