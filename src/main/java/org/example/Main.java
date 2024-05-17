16c5c9076733120f2722171e6caba520965e37c2
bilsina <bils1n@icloud.com>
2024-05-17 14:15:53 +0500

    added subtract

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,14 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println(sum(1, 4));
         System.out.println(sum(1, 10));
+        System.out.println(subtract(100, 10));
     }
 
     public static int sum(int a, int b) {
         return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
